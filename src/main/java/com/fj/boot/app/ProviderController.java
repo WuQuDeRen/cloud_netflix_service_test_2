c6a11cc381e889f2cfcee2767137c80c9623d04a
WoLaiZheLiLe <2821003945@qq.com>
2019-09-13 20:30:36 +0800

    no message

---
+++ src/main/java/com/fj/boot/app/ProviderController.java
@@ -0,0 +1,14 @@
+package com.fj.boot.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProviderController {
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return "hello，接口请求好了";
+    }
+}
+
